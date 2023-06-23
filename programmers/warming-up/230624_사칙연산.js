4f22131dd4700189b4027328e8f6a02d43386642
EthanKwon <ethkwon94@gmail.com>
2023-06-24 02:05:52 +0900

    [programmers] 몸풀기 : 사칙연산

---
+++ programmers/warming-up/230624_사칙연산.js
@@ -0,0 +1,52 @@
+
+/* 간단한 몸풀기 */
+
+/**
+ * 두 수의 합
+ * 
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns 
+ */
+function solution(num1, num2) {
+    const answer = num1 + num2;
+    return answer;
+}
+
+/**
+ * 두 수의 차
+ * 
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns 
+ */
+function solution(num1, num2) {
+    const answer = num1 - num2;
+    return answer;
+}
+
+
+/**
+ * 두 수의 곱
+ * 
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns 
+ */
+function solution(num1, num2) {
+    const answer = num1 * num2;
+    return answer;
+}
+
+
+/**
+ * 몫 구하기
+ * 
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns 
+ */
+function solution(num1, num2) {
+    const answer = Math.trunc(num1 / num2);
+    return answer;
+}
